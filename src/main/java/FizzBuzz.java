3b2120d3de216231119bafd11b6b7a15c0541081
pb97582 <bhoitepallavi@johndeere.com>
2018-09-17 13:54:08 +0530

    feat: add fizzbuzz kata by TDD

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,6 +1,35 @@
 public class FizzBuzz {
 
+    public static final int FIZZ_NUMBER = 3;
+    public static final int BUZZ_NUMBER = 5;
+
+
     public String process(Integer inputValue) {
-       return inputValue.toString();
+        String outputValue = buildFizzValue(inputValue);
+        outputValue += buildBuzzValue(inputValue);
+        return buildOutputValue(inputValue, outputValue);
+    }
+
+    private String buildOutputValue(Integer inputValue, String outputValue) {
+        if ("".equals(outputValue)) {
+            outputValue = inputValue.toString();
+        }
+        return outputValue;
+    }
+
+    private String buildBuzzValue(Integer inputValue) {
+        String outputValue = "";
+        if (inputValue % BUZZ_NUMBER == 0) {
+            outputValue = "buzz";
+        }
+        return outputValue;
+    }
+
+    private String buildFizzValue(Integer inputValue) {
+        String outputValue = "";
+        if (inputValue % FIZZ_NUMBER == 0) {
+            outputValue = "fizz";
+        }
+        return outputValue;
     }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,14 +1,51 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.time.format.TextStyle;
 
 public class FizzBuzzTest {
 
-	@Test
+    private FizzBuzz fizzBuzz;
+
+    @Before
+    public void setUp() {
+        fizzBuzz = new FizzBuzz();
+    }
+
+    @Test
     public void shouldReturn1for1() {
-		FizzBuzz fizzBuzz = new FizzBuzz();
-        String actualReturnValue = fizzBuzz.process(1);
-        String expectedReturnValue = "1";
-        assertEquals(expectedReturnValue, actualReturnValue);
+        assertEquals("1", fizzBuzz.process(1));
+    }
+
+    @Test
+    public void shouldReturnFizzFor3() {
+        assertEquals("fizz", fizzBuzz.process(3));
+    }
+
+    @Test
+    public void shouldReturnBuzzFor5() {
+        assertEquals("buzz", fizzBuzz.process(5));
+    }
+
+    @Test
+    public void shouldReturnFizzFor6() {
+        assertEquals("fizz", fizzBuzz.process(6));
+    }
+
+    @Test
+    public void shouldReturnBuzzFor10() {
+        assertEquals("buzz", fizzBuzz.process(10));
+    }
+
+    @Test
+    public void shouldReturnFizzBuzzFor15() {
+        assertEquals("fizzbuzz", fizzBuzz.process(15));
+    }
+
+    @Test
+    public void shouldReturnFizzForMinus3() {
+        assertEquals("fizz", fizzBuzz.process(-3));
     }
 }
